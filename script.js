fce65cc16bc92176a5fa64ae41df0f53cdd88fe8
iansantos4352 <149393170+iansantos4352@users.noreply.github.com>
2023-11-25 23:25:27 +0000

    introducing script on html page.

---
+++ script.js
@@ -0,0 +1,22 @@
+// Abaixo, uma descrição de alimentos e sua quantidade de calorias (Kcal) para cada 100g de alimento.
+
+// Carboidratos:
+
+let arroz = 130;
+let milho = 98;
+let batataDoce = 85;
+let macarrao = 130;
+let banana = 90;
+
+// Proteínas:
+
+let frangoGrelhado = 170;
+let patinho = 146;
+let ovoCozido = 155;
+let tilapia = 105
+let figado = 192
+
+// Gorduras
+
+const carboidratos = [arroz, milho, batataDoce, macarrao, banana];
+const proteinas = [frangoGrelhado, patinho, ovoCozido, tilapia, figado];
