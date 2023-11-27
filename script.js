e857871522ff7e82a8872b95d229d62f0b17a426
iansantos4352 <149393170+iansantos4352@users.noreply.github.com>
2023-11-27 20:38:38 +0000

    script refatored and add any foods at carbos and proteins

--- script.js
+++ script.js
@@ -1,22 +1,54 @@
 // Abaixo, uma descrição de alimentos e sua quantidade de calorias (Kcal) para cada 100g de alimento.
 
-// Carboidratos:
+// Carbos:
 
-let arroz = 130;
-let milho = 98;
-let batataDoce = 85;
-let macarrao = 130;
-let banana = 90;
+let cookedRice = "100g of Cooked Rice = " + 130 + " Kcal";
+let cookedCorn = "100g of Cooked Corn = " + 98 + " Kcal";
+let cookedSweetPotato = "100g of Cooked Sweet Potato = " + 85 + " Kcal";
+let cookedNoodle = "100g of Cooked Noodle = " + 130 + " Kcal";
+let freshBananas = "100g of Fresh Bananas = " + 90 + " Kcal";
 
-// Proteínas:
+console.log(" - Here is an list of 5 carbos: \n\n"
+ + cookedRice + "\n"
+ + cookedCorn + "\n"
+ + cookedSweetPotato + "\n"
+ + cookedNoodle + "\n"
+ + freshBananas);
 
-let frangoGrelhado = 170;
-let patinho = 146;
-let ovoCozido = 155;
-let tilapia = 105
-let figado = 192
+// Protein:
 
-// Gorduras
+let grilledChicken = "100g of Grilled Chicken = " + 170 + " Kcal";
+let grilledDuckling = "100g Grilled Duckling = " + 146 + " Kcal";
+let cookedEgg = "100g of Cooked Egg = " + 155 + " Kcal";
+let grilledTilapia = "100g of Grilled Tilapia = " + 105 + " Kcal";
+let grilledLiver = "100g of Grilled Liver = " + 192 + " Kcal";
 
-const carboidratos = [arroz, milho, batataDoce, macarrao, banana];
-const proteinas = [frangoGrelhado, patinho, ovoCozido, tilapia, figado];
+console.log(" - Here is an list of 5 proteins: \n\n"
++ grilledChicken + "\n"
++ grilledDuckling + "\n"
++ cookedEgg + "\n"
++ grilledTilapia + "\n"
++ grilledLiver)
+
+// Fat
+
+// 100g / Foods by calorie
+
+const cookedRiceKcal = 130;
+const cookedCornKcal = 98;
+const cookedSweetPotatoKcal = 85;
+const cookedNoodleKcal = 130;
+const freshBananasKcal =90;
+
+const grilledChickenKcal = 170;
+const grilledDucklingKcal = 146;
+const cookedEggKcal = 155;
+const grilledTilapiaKcal = 105;
+const grilledLiverKcal = 192;
+
+let meet1 = "200g of Cooked Rice = " + (cookedRiceKcal * 2) + " Kcal \n" +
+"150g of Grilled Chucken = " + (grilledChickenKcal * 1.5) + " Kcal";
+
+
+
+console.log(" - First meal: \n\n" + meet1);
